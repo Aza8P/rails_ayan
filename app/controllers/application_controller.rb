420aaeed4dd48a7124c1d8e15df91fb9550cd52d
Aza8P <a3jargal@icloud.com>
2023-05-09 14:06:34 +0800

    language switcher is finally working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { locale: I18n.locale }
   end
+
+  def locale
+    locale = params[:locale].to_sym
+    I18n.locale = locale if I18n.available_locales.include?(locale)
+    head :ok
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,31 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
 import "controllers"
+import "@hotwired/turbo-rails"
 import "bootstrap"
-import Popper from "@popperjs/core"
-import "jquery"
+import $ from 'jquery';
 
-console.log("Hello from application.js")
+
+console.log("Hello from application.js");
+console.log("JAVASCRIPT PACKS LOADED");
+
+document.addEventListener("turbo:load", () => {
+  const activeLanguageLink = document.querySelector(".language-link.selected");
+  const languageLinks = document.querySelectorAll(".language-link");
+  languageLinks.forEach((languageLink) => {
+    languageLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      const language = languageLink.dataset.locale;
+      const csrfToken = document.querySelector("meta[name='csrf-token']").content;
+      fetch(`/locale?locale=${language}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRF-Token": csrfToken,
+        },
+      }).then(() => {
+        window.location.reload();
+      });
+    });
+  });
+});
+
