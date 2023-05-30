e049721b31720c2eda0f5acd3458fc9312a10596
Aza8P <a3jargal@icloud.com>
2023-05-30 14:53:24 +0800

    language switcher is fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,19 @@
 class ApplicationController < ActionController::Base
   include Devise::Controllers::Helpers
+  before_action :set_locale
 
+
+  private 
+  
   def set_locale
+    # Check if the locale is present in the params
     if params[:locale].present? && I18n.available_locales.include?(params[:locale].to_sym)
-      cookies.permanent[:locale] = params[:locale].to_sym
-    else
-      I18n.locale = cookies[:locale] || I18n.default_locale
+      # Store the locale in the session
+      session[:locale] = params[:locale].to_sym
     end
-  end
 
-  def default_url_options
-    { locale: I18n.locale }
+    # Set the locale based on the stored value in the session or fallback to the default locale
+    I18n.locale = session[:locale] || I18n.default_locale
+  
   end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-  helper_method :current_user
-
 end
