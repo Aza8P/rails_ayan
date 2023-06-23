4afe225d5d5800ff074268bda0bed8bf60c9a3c9
Aza8P <a3jargal@icloud.com>
2023-06-23 13:02:52 +0800

    fixed the travel preference form & added toggle logic with stimulus

--- db/migrate/20230427112549_create_users.rb
+++ db/migrate/20230427112549_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :password_digest
       t.string :name
       t.string :location
-      t.date :created_at
 
       t.timestamps
     end
